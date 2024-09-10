b3467cfef0c0e36a56a3ca760ea86d1862fd0299
Ravi Shankar Rajupalepu <148464895+Ravi4090@users.noreply.github.com>
2024-09-10 10:40:59 +0530

    Update setup_docker_loki.sh

--- setup_docker_loki.sh
+++ setup_docker_loki.sh
@@ -5,40 +5,12 @@
 # This script installs the Loki Docker plugin, updates Docker's log driver to Loki,
 # and restarts Docker while preserving the state of running containers.
 
-# Path to your .env file
-ENV_FILE=".env"
-
-# Source the .env file to get the Loki URL
-if [ -f "$ENV_FILE" ]; then
-  export $(grep -v '^#' "$ENV_FILE" | xargs)
-else
-  echo "Environment file .env not found!"
-  exit 1
-fi
-
-# Validate the required environment variable
-if [ -z "$LOKI_URL" ]; then
-  echo "LOKI_URL is not set in the .env file!"
-  exit 1
-fi
 
 # Install the Loki Docker plugin
 echo "Installing Loki Docker plugin..."
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
 
-# Update Docker daemon configuration
-DAEMON_JSON="/etc/docker/daemon.json"
-echo "Updating Docker daemon configuration..."
-sudo tee "$DAEMON_JSON" > /dev/null <<EOL
-{
-    "debug": true,
-    "log-driver": "loki",
-    "log-opts": {
-        "loki-url": "$LOKI_URL",
-        "loki-batch-size": "400"
-    }
-}
-EOL
+
 
 # Define a temporary file to store container names
 TEMP_FILE=$(mktemp)
